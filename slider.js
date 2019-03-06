f1c1525f441fad83e3744e7b7720f30cdd1f8936
MarkoM91 <markomedic91@gmail.com>
2019-03-06 16:12:35 +0100

    commit4

--- slider.js
+++ slider.js
@@ -1,5 +1,29 @@
-function init() {
+function leftClick() {
+  console.log("left-click")
+}
 
+function rightClick() {
+
+  var activeImg = $('img.active');
+  activeImg.removeClass("active");
+
+  var nextImg = activeImg.next("img");
+  if (activeImg.hasClass("last")) {
+
+    nextImg = $('.picture-container > img.first');
+  }
+  nextImg.addClass("active");
+}
+
+
+
+
+function init() {
+var leftPart = $('.left-part');
+var rightPart = $('.right-part');
+
+leftPart.click(leftClick);
+rightPart.click(rightClick);
 }
 init();
 
